c7fc16478d47bce713acc27eb19d02bbc22d6aa9
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-01-08 18:28:50 +0100

    almost done

--- D2-exercise/provided_code/doplot.py
+++ D2-exercise/provided_code/doplot.py
@@ -1,56 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("plasma_timing.dat")
+data  = np.loadtxt("plasma_timing.dat")
+ndata = np.loadtxt("scaling_timing.dat")
 
 size_m = data[:,0]
 sec    = data[:,1]
+nsec   = ndata[:,1]
+rep    = 6     # number of repetition for each matrix size
+lvec   = len(data) / rep
 
-rep = 6     # number of repetition for each matrix size
-lvec = len(data) / rep
+s  = np.zeros(lvec)
+t  = np.zeros(lvec)
+nt = np.zeros(lvec - 1)
 
-s = np.zeros(lvec)
-t = np.zeros(lvec)
 err = np.zeros(lvec)
-speedup = np.zeros(lvec)
+nerr = np.zeros(lvec - 1)
+speedup  = np.zeros(lvec)
+nspeedup = np.zeros(lvec - 1)
 
 i = 0
 j = 0
 count = 0
 t_tmp = 0.
+n_tmp = 0.
 
 while i < len(sec):
     while j < rep:
         t_tmp += sec[i + j]
+        if i > 0:
+            n_tmp += nsec[i + j - rep]
         j += 1
 
 
     s[count] = size_m[i]
     t[count] = t_tmp / rep
-
+    if i > 0:
+        nt[count - 1] = n_tmp / rep
+    
     t_tmp = 0.
+    n_tmp = 0.
     j -= rep
 
     while j < rep:
         t_tmp += (sec[i + j] - t[count])**2
+        if i > 0:
+            n_tmp += (nsec[i + j - rep] - nt[count - 1])**2
         j += 1
 
     err[count] = (t_tmp / (rep - 1.))**0.5
-
+    if i > 0:
+        nerr[count - 1] = (n_tmp / (rep - 1.))**0.5
+    
     t_tmp = 0.
+    n_tmp = 0.
     count += 1
     i += rep
     j = 0
 
-speedup = t[0] / t
+speedup  = t[0] / t
+# We have to multiply by factor 2 to allow comparison with PLASMA data
+# (supposing a linear scaling for NP = 2)
+nspeedup = 2. * nt[0] / nt
 
 plt.figure()
-# plt.plot(s, t)
 plt.errorbar(s, speedup, yerr=err, label = 'PLASMA')
+plt.errorbar(s[1:], nspeedup, yerr=nerr, label = 'ScaLAPACK')
 plt.plot(s, s, label = 'Teor')
-plt.title('Speedup for Size = 8000')
+plt.title('Speedup for matrix size = 8000')
 plt.xlabel('# Threads')
 plt.ylabel('Speedup')
-plt.legend(bbox_to_anchor = (.3, 1.))
+plt.legend(bbox_to_anchor = (.35, 1.))
 plt.show()
 # plt.savefig('timing.png')

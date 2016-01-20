5d7a79887825684fcece4e6292c07c99ff40c7cd
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-01-20 11:17:05 +0100

    add scaling_plasma_elcid.png

--- D2-exercise/provided_code/elcid/increasing_size.py
+++ D2-exercise/provided_code/elcid/increasing_size.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 data  = np.loadtxt("plasma_timing.dat.10000")
 data2  = np.loadtxt("plasma_timing.dat.18000")
 data3  = np.loadtxt("plasma_timing.dat.20000")
-# threads = np.loadtxt("plasma_timing.safe")
-# size_m = threads[:,0]
-
-# data = np.loadtxt("4000new_timing.dat")
-# data2 = np.loadtxt("scaling_timing.dat")
-# data3 = np.loadtxt("10new_timing.dat")
 
 size_m = data[:,0]
 sec1   = data[:,1]
@@ -39,25 +33,18 @@ t_tmp3 = 0.
 
 while i < len(sec1):
     while j < rep:
-        # t_tmp1 += sec1[i + j]
-        # t_tmp2 += sec2[i + j]
-        # t_tmp3 += sec3[i + j]
-
-        t_tmp1 += sec1[i + j - rep]
-        t_tmp2 += sec2[i + j - rep]
-        t_tmp3 += sec3[i + j - rep]
+        t_tmp1 += sec1[i + j]
+        t_tmp2 += sec2[i + j]
+        t_tmp3 += sec3[i + j]
 
         j += 1
 
 
     s[count]  = size_m[i]
-    # t1[count] = t_tmp1 / rep
-    # t2[count] = t_tmp2 / rep
-    # t3[count] = t_tmp3 / rep
 
-    t1[count - 1] = t_tmp1 / rep
-    t2[count - 1] = t_tmp2 / rep
-    t3[count - 1] = t_tmp3 / rep
+    t1[count] = t_tmp1 / rep
+    t2[count] = t_tmp2 / rep
+    t3[count] = t_tmp3 / rep
 
     t_tmp1 = 0.
     t_tmp2 = 0.
@@ -65,23 +52,15 @@ while i < len(sec1):
     j -= rep
 
     while j < rep:
-        # t_tmp1 += (sec1[i + j] - t1[count])**2
-        # t_tmp2 += (sec2[i + j] - t2[count])**2
-        # t_tmp3 += (sec3[i + j] - t3[count])**2
-
-        t_tmp1 += (sec1[i + j - rep] - t1[count - 1])**2
-        t_tmp2 += (sec2[i + j - rep] - t2[count - 1])**2
-        t_tmp3 += (sec3[i + j - rep] - t3[count - 1])**2
+        t_tmp1 += (sec1[i + j] - t1[count])**2
+        t_tmp2 += (sec2[i + j] - t2[count])**2
+        t_tmp3 += (sec3[i + j] - t3[count])**2
 
         j += 1
 
-    # err1[count] = (t_tmp1 / (rep - 1.))**0.5
-    # err2[count] = (t_tmp2 / (rep - 1.))**0.5
-    # err3[count] = (t_tmp3 / (rep - 1.))**0.5
-
-    err1[count - 1] = (t_tmp1 / (rep - 1.))**0.5
-    err2[count - 1] = (t_tmp2 / (rep - 1.))**0.5
-    err3[count - 1] = (t_tmp3 / (rep - 1.))**0.5
+    err1[count] = (t_tmp1 / (rep - 1.))**0.5
+    err2[count] = (t_tmp2 / (rep - 1.))**0.5
+    err3[count] = (t_tmp3 / (rep - 1.))**0.5
     
     t_tmp1 = 0.
     t_tmp2 = 0.
@@ -97,13 +76,12 @@ speedup3 = t3[0] / t3
 plt.figure()
 plt.plot(s, s, label = 'Theor')
 plt.errorbar(s, speedup3, yerr=err3, label = '10000x10000')
-plt.errorbar(s, speedup2, yerr=err2, label = '8000x8000')
-plt.errorbar(s, speedup1, yerr=err1, label = '4000x4000')
+plt.errorbar(s, speedup2, yerr=err2, label = '18000x18000')
+plt.errorbar(s, speedup1, yerr=err1, label = '20000x20000')
 
-plt.title('Speedup for PLASMA library (ULISSE)')
-plt.title('Speedup for ScaLAPACK library (ULISSE)')
+plt.title('Speedup for PLASMA library (ELCID)')
 plt.xlabel('# Threads')
 plt.ylabel('Speedup')
 plt.legend(bbox_to_anchor = (.38, 1.))
-plt.show()
-# plt.savefig('scaling_scalapack.png')
+# plt.show()
+plt.savefig('scaling_plasma_elcid.png')
